3074e2505976bf5259a4995e4348577aa5789342
Ryan Graham <rg@ryangraham.ca>
2024-10-12 16:00:03 -0400

    expired token should be a 401, not a 403.

--- server.js
+++ server.js
@@ -262,7 +262,7 @@ const authenticateToken = (req, res, next) => {
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(401);
     req.user = { id: user.id, accountLevel: user.accountLevel };
     next();
   });
